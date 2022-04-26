2fffaa612cd72236080e957353c94093b2190b05
analytics-tlc <95776923+analytics-tlc@users.noreply.github.com>
2022-04-26 13:32:04 -0400

    Update code.py
    Nikita added decoding code

--- code.py
+++ code.py
@@ -36,18 +36,51 @@ from requests import Session, exceptions
 # using this until Phillip completes his part
 active_fhvs = pd.read_csv("I:\\COF\\COF\\_DA&E_\\Nikita\\Python_Projects\\testing_git\\data.csv")
 
-# Chris - filter out everything but SHLs. SHLs are the ones that have permit number. End result should be a list shl of vins called vins
+# Chris - filter out everything but SHLs. SHLs are the ones that have permit number. End result should be a list of shl vins called vins
 
 shl= active_fhvs.loc[active_fhvs['Permit License Number'].notnull()]
 shl_permits = shl['Vehicle VIN Number']
-vins = str(list(shl_permits.astype(str)))[1:-1]
+#vins = str(list(shl_permits.astype(str)))[1:-1]
 #test
 
-
+vins = shl['Vehicle VIN Number'].to_list()
 # Nikita - Decode that vins and return a dataframe with the vehicle info.
 
+# Yield successive n-sized 
+# chunks from l. 
+def divide_chunks(l, n): 
+      
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
+  
+# How many elements each 
+# list should have 
+n = 30
+  
+vins_chunks = list(divide_chunks(vins, n))
 
 
+df = pd.DataFrame()
+
+
+for i in vins_chunks:
+
+
+
+    my_string = ';'.join(i)
+
+    print(my_string)
+    s = Session()
+    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/'
+    s.mount(url, HTTPAdapter(max_retries=Retry(total=2, backoff_factor=1, method_whitelist=frozenset(['GET', 'POST']), status_forcelist=[ 500, 502, 503, 504, 521])))
+    post_fields = {'format': 'json', 'data':f'{my_string}'}
+    r = requests.post(url, data=post_fields)
+    print(r.status_code)
+    x = r.json()
+    data = pd.DataFrame(x['Results'])[['VIN','Make','Model','ModelYear','BodyClass']]
+    df = df.append(data)
+    print('done')
 
 # John - Rename body classes so that we know how many sedans there are. 
 # Final result should be a dataframe with 3 columns: industry, body_type, count
